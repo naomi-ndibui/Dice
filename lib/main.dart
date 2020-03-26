86753a914749605aba04403a9c7f9901604963c2
Naomi <naomi.ndibui@gmail.com>
2020-03-26 15:24:22 +0300

    onPressed action,dice var and datatype, Stateless widget to Stateful widget and randomisig the dice

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +17,22 @@ void main() {
         ),
       ));
 }
-class DicePage extends StatelessWidget{
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context){
     return Center(
@@ -23,12 +40,22 @@ class DicePage extends StatelessWidget{
         children: <Widget>[
           Expanded(
               child: FlatButton(
-                child: Image.asset('images/dice1.png'),
+                child: Image.asset(
+                    'images/dice$leftDiceNumber.png',
+                ),
+                onPressed: () {
+                  changeDiceFace();
+                },
               ),
          ),
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset(
+                  'images/dice$rightDiceNumber.png',
+              ),
+              onPressed: (){
+                changeDiceFace();
+              },
             ),
           ),
         ],
